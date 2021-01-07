2aba9baec962259e9ec72efa0abb8d28e4845456
Mustafa <69982652+Kaves47@users.noreply.github.com>
2021-01-07 17:20:21 +0300

    Add files via upload

---
+++ kaves_npcs/client.lua
@@ -0,0 +1,49 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+Citizen.CreateThread(function()
+	Citizen.Wait(5)
+			for _, v in pairs(config.NPCs) do
+				RequestModel(GetHashKey(v.model))
+				while not HasModelLoaded(GetHashKey(v.model)) do
+					Wait(1)
+				end
+				local npc = CreatePed(4, v.model, v.coords, v.heading,  false, true)
+				local npctext = (v.coords)
+				SetPedFleeAttributes(npc, 0, 0)
+				SetPedDropsWeaponsWhenDead(npc, false)
+				SetPedDiesWhenInjured(npc, false)
+				SetEntityInvincible(npc , true)
+				FreezeEntityPosition(npc, true)
+				SetBlockingOfNonTemporaryEvents(npc, true)
+			end
+end)
+
+
+function DrawText3D(x,y,z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    SetTextScale(0.30, 0.30)
+    SetTextFont(0)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    local factor = (string.len(text)) / 250
+    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 140)
+end
+
+
+
+
+
+
+
---
+++ kaves_npcs/config.lua
@@ -0,0 +1,14 @@
+config = {}
+
+
+
+config.NPCs = {
+	[1] = {model ="hc_gunman", coords = vector3(-2165.91,5198.03,15.88), heading = 114.98},
+	[2] = {model ="csb_chef", coords = vector3(-1171.08,-1571.14,3.66), heading = 121.03},
+	[3] = {model ="g_m_importexport_01", coords = vector3(2328.16,2569.93,45.68), heading = 332.96},
+	[4] = {model ="g_m_m_chemwork_01", coords = vector3(1393.06,3606.16,37.94), heading = 51.86},
+	[5] = {model ="g_m_m_chicold_01", coords = vector3(-472.68,6288.56,12.71), heading = 221.16},
+	[6] = {model ="g_m_m_chicold_01", coords = vector3(1234.88,-2917.98,28.69), heading = 315.63},
+	[7] = {model ="a_m_m_golfer_01", coords = vector3(-97.12, -1013.8, 26.3), heading = 155.26},
+}
+
