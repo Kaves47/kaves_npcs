a825226006eac4ed47f70120ffa39d14e60f4df0
Mustafa <69982652+Kaves47@users.noreply.github.com>
2021-02-01 23:00:03 +0300

    Update client.lua

--- kaves_npcs/client.lua
+++ kaves_npcs/client.lua
@@ -26,21 +26,6 @@ Citizen.CreateThread(function()
 end)
 
 
-function DrawText3D(x,y,z, text)
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
-    SetTextScale(0.30, 0.30)
-    SetTextFont(0)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(1)
-    AddTextComponentString(text)
-    DrawText(_x,_y)
-    local factor = (string.len(text)) / 250
-    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 140)
-end
-
 
 
 
